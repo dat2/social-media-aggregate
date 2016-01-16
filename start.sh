bf4a2094f771d662f44db92fe8f3a6bf3423edf8
Nicholas Dujay <ndujay05@ca.ibm.com>
2016-01-15 22:35:29 -0500

    Background and trap yay

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
+# https://stackoverflow.com/questions/9023164/in-bash-how-can-i-run-multiple-infinitely-running-commands-and-cancel-them-all
 cd client
 react-dev-server &
+PIDS[0]=$!
 
 #cd ../server
-#npm run watch
+#npm run watch &
+#PIDS[1]=$!
+
+trap "kill -9 ${PIDS[*]}" SIGINT
+wait
