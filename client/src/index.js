2bed511bb65aa0ddd0d5b80ec968f9a981d80e19
Kevin Ryczko <kevinryczko@me.com>
2016-01-16 13:22:55 -0500

    pushing huge mess

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+var $ = require('jquery');
 
-ReactDOM.render(<p> Hello World </p>, document.getElementById('react'));
+class SignInToTwitter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            requestToken: '', requestTokenSecret: ''
+        }
+    }
+
+    render() {
+        $.post('/twitterInfo', function (data) {
+            console.log(data);
+        });
+        return <div> Hello World!!! </div>
+    }
+}
+
+ReactDOM.render(<SignInToTwitter/>, document.getElementById('react'));
--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
+var twitterAPI = require('node-twitter-api');
 
-var PORT = 3000;
+var twitter = new twitterAPI({
+    consumerKey: 'aAG0jYGwxKSHwdQ2tSfDRMskR',
+    consumerSecret: '3w2JOgJDHulIXZtdB1WZPjJZXjbLNGkH0eG2H1RNehIyPDVTIV',
+    callback: 'http://localhost:8080'
+});
+
+app.post('/twitterInfo', function(req, res) {
+    twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+        if (error) {
+            console.log("Error getting OAuth request token : " + error);
+        } else {
+            res.send({requestToken: requestToken, requestTokenSecret: requestTokenSecret});
+        }
+    });
+});
+
+var PORT = 8080;
 
 app.listen(PORT, function() {
   console.log('listening on port ' + PORT);
